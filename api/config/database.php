73cfc55a5c4b071f2e520167845d5dd7ccfd91fd
Justin Dannemiler <jdannem6@kent.edu>
2022-04-26 12:14:28 -0400

    Add files via upload

--- api/config/database.php
+++ api/config/database.php
@@ -1,26 +1,34 @@
 <?php
+
 class Database{
- 
-    // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "hospital_db";
-    private $username = "root";
-    private $password = "";
+    
+    // specify the credentials for the database
+    private $host= "localhost";
+    private $db_name= "hrmanagement";
+    private $username= "root";
+    private $password="";
     public $conn;
- 
-    // get the database connection
+
+    // define a function for creating a connection with database
     public function getConnection(){
- 
-        $this->conn = null;
- 
-        try{
+        $this->conn = null; // Initialize the connection as null
+
+        // Try creating a connection with the specified credentials
+        try {
+            // Create new PHP Data Object (PDO) comprised of the connection elements
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            //Set up the connection through the connection object
             $this->conn->exec("set names utf8");
-        }catch(PDOException $exception){
+        }
+        // If connection can't be established, give connection erro message
+        catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
- 
+
+        // Return the connection object 
         return $this->conn;
     }
+    
 }
+
 ?>
